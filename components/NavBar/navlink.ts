16f2756bc85537a36a926eca7a16302adc3b173c
Fridolin Austin <fridoustin@Fridolins-Laptop.local>
2024-11-13 02:08:11 +0800

    making folder

---
+++ components/NavBar/navlink.ts
@@ -0,0 +1,18 @@
+export const Navlink = [
+    {
+        title: "Home",
+        href: "/home"
+    },
+    {
+        title: "About",
+        href: "/about"
+    },
+    {
+        title: "Class",
+        href: "/class"
+    },
+    {
+        title: "Membership",
+        href: "/membership"
+    }
+];
