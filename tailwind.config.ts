aba1790464cce2d0b260a6402ea26d8ef5e6d5e3
afdalfauzan <afdalfauzan2109@gmail.com>
2024-11-14 02:15:24 +0800

    style: home page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'hero-gym': "url('/images/hero-gym.jpg')",  
+      },
     },
   },
   plugins: [
